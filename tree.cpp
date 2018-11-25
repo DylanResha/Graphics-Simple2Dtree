d485c7c718b4394e6df2addd040f85f67b956d4a
Dylan Resha <dylanresha@gmail.com>
2018-11-25 15:08:02 -0500

    reupload of pushing things to anvil with SHPC commented out

--- tree.cpp
+++ tree.cpp
@@ -38,7 +38,6 @@ struct triangle{
 
 //vectors to hold my lists in
 vector<vertex> coords;
-vector<vertex> cCoords;
 vector<line> lineList;
 vector<triangle> triList;
 
@@ -50,9 +49,6 @@ float SCALE = 1.0;
 float rp = 1.0;
 float tX = -WINDOW_MAX/2;
 float tY = -WINDOW_MAX/2;
-
-
-
 
 void vmatm (int SIZE, float *pA, float *pB)
 
@@ -142,6 +138,8 @@ void SHPC(vertex *inVertexArray, vertex *outVertexArray, int inLength, int *outL
 	}
 }
 
+
+
 /////////////////////////////////////////////////////////////////////
 //tessalation/////
 //////////////////////////////////////////////////////////////////
@@ -188,7 +186,7 @@ bool polyIntersect(){
 		//cout<<"size of line list"<<lineList.size()<<endl;
 	   int j = (i+1);	
 	 for(int k=0; k<lineList.size()-3; k++){
-		//cout<<"in poly intersect loop"<<endl;
+		cout<<"in poly intersect loop"<<endl;
 		if(checkIntersect(i,j%lineList.size())){return true;}
 		j++;
  	  }
@@ -375,19 +373,6 @@ triList.push_back(lastTri);
 
 
 
-void clip(){
- cCoords = coords;
-
- 
-    for(int i = 0; i<cCoords.size();i++){
-	if(cCoords.at(i).x<100){cCoords.at(i).x=100;}
-	if(cCoords.at(i).y<100){cCoords.at(i).y=100;}
-	if(cCoords.at(i).x>900){cCoords.at(i).x=900;}
-	if(cCoords.at(i).y>900){cCoords.at(i).y=900;}
-	
-}
-}
-
 
 
 
@@ -479,7 +464,7 @@ void PipeLine( float *vp, int vpts )
     /* Don't forget to initialize the ponter! */
     TM = &TransformationMatrix[0];
     // Clip tree  	
-	
+	//SHPC(vp, vp, vpts, vpts, );
 
 
     // Translate to origin  
@@ -630,21 +615,21 @@ void toVertex ( float *apts, struct vertex *vp, int pts )
 
 }
 
-void drawTree()
+void drawTree(int points)
 {
 line temp;
 glBegin(GL_LINES);
-    for (int i=0;i<cCoords.size()-1;i++){
-        glVertex2f( cCoords.at(i).x, cCoords.at(i).y );
-	glVertex2f( cCoords.at(i+1).x, cCoords.at(i+1).y);
-	temp.a = cCoords.at(i);
-	temp.b = cCoords.at(i+1);
+    for (int i=0;i<points-6;i++){
+        glVertex2f( coords.at(i).x, coords.at(i).y );
+	glVertex2f( coords.at(i+1).x, coords.at(i+1).y);
+	temp.a = coords.at(i);
+	temp.b = coords.at(i+1);
 	lineList.push_back(temp);
 	}
- 	glVertex2f( cCoords.back().x, cCoords.back().y );
-	glVertex2f( cCoords.front().x, cCoords.front().y);
-	temp.a = cCoords.back();
-	temp.b = cCoords.front();
+ 	glVertex2f( coords.back().x, coords.back().y );
+	glVertex2f( coords.front().x, coords.front().y);
+	temp.a = coords.back();
+	temp.b = coords.front();
 	lineList.push_back(temp);
 
     glEnd();
@@ -697,14 +682,13 @@ void display( void )
     /* Now start the process of rotating */
     PipeLine( apts, inPoints );
     toVertex( apts, invp, inPoints );
-	
+
     glColor3f(1.0, 0.0, 0.0);
 
     /* Draw Scaled and Rotated Arrow */
     //drawTrunk( invp, 6 );
     //drawLeaves(invp, inPoints);
-    clip();
-    drawTree();
+    drawTree(inPoints);
     glutSwapBuffers();
 
  }
